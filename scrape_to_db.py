01cbd6d87482e76e8aacda01c26fcbf4f3b92a13
Juandabi <jdavidcardonam@gmail.com>
2022-08-07 00:16:33 -0500

    Ready to deploy Create new branch

--- scrape_to_db.py
+++ scrape_to_db.py
@@ -8,27 +8,32 @@ from scrapping import get_last_page as get_last_page
 from scrapping import iterate_jobs_in_page as iterate_jobs_in_page
 
 def main():
+    try:
     # Get connection to database
-    engine = engine_db()
-    #Get days to scrap and url
-    days = get_days(engine)
-    print(f'Days to scrap: {days}')
-    url = f'https://findajob.dwp.gov.uk/search?pp=50&f={days}'
-    #Scrapping the website
-    last_page =  int(get_last_page(url))
-    print(f'Start scrapping total pages: {last_page}')
-    # Iterate through all the pages
-    for page in range (last_page, 0, -1):
-        print('-------------------------------------------------------------')
-        print(f'Start scrapping page: {page}')
-        page_url = f'{url}&page={page}'
-        #scrap each page and save the data in a dataframe
-        jobs_details_for_page = iterate_jobs_in_page(page_url)
-        # jobs_details_for_page.fillna('None', inplace=True)
-        #Save data from each page to db
-        save_to_db(jobs_details_for_page,engine)
-        print(f'| page {page} is saved |')
-    print('Scrapping is done')
+        engine = engine_db()
+        #Get days to scrap and url
+        days = get_days(engine)
+        print(f'Days to scrap: {days}')
+        url = f'https://findajob.dwp.gov.uk/search?pp=50&f={days}'
+        #Scrapping the website
+        last_page = int(get_last_page(url))
+        print(f'Start scrapping total pages: {last_page}')
+        # Iterate through all the pages
+        for page in range (last_page, 0, -1):
+            print('-------------------------------------------------------------')
+            print(f'Start scrapping page: {page}')
+            page_url = f'{url}&page={page}'
+            #scrap each page and save the data in a dataframe
+            jobs_details_for_page = iterate_jobs_in_page(page_url)
+            # jobs_details_for_page.fillna('None', inplace=True)
+            #Save data from each page to db
+            save_to_db(jobs_details_for_page,engine)
+            print(f'| page {page} is saved |')
+        print('Scrapping is done')
+    except Exception as e:
+        print(e)
+        print('Scrapping is failed')
+        raise e
 
 
 #%%
