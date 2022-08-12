837f3f956bff12855313ad3a10ee600774db8024
Juan David Cardona <jdavidcardonam@gmail.com>
2022-08-12 05:06:59 +0000

    Move to folder app

--- Dockerfile
+++ Dockerfile
@@ -1,24 +1,11 @@
-# For more information, please refer to https://aka.ms/vscode-docker-python
-FROM python:3.9.12
+FROM tiangolo/uvicorn-gunicorn-fastapi:python3.9
 
-EXPOSE 8000
+COPY ./requirements.txt /app/requirements.txt
 
-# Keeps Python from generating .pyc files in the container
-ENV PYTHONDONTWRITEBYTECODE=1
+RUN pip install --no-cache-dir --upgrade -r /app/requirements.txt
 
-# Turns off buffering for easier container logging
-ENV PYTHONUNBUFFERED=1
+COPY ./app /app/app
 
-# Install pip requirements
-COPY requirements.txt .
-RUN python -m pip install -r requirements.txt
 
-WORKDIR /app
-COPY . /app
-
-# Creates a non-root user with an explicit UID and adds permission to access the /app folder
-# For more info, please refer to https://aka.ms/vscode-docker-python-configure-containers
-RUN adduser -u 5678 --disabled-password --gecos "" appuser && chown -R appuser /app
-USER appuser
-# During debugging, this entry point will be overridden. For more information, please refer to https://aka.ms/vscode-docker-python-debug
-CMD ["gunicorn", "-b", ":8000", "-k", "uvicorn.workers.UvicornWorker", "main:app"]
+#
+CMD ["uvicorn", "app.main:app", "--proxy-headers", "--host", "0.0.0.0", "--port", "8000","--root-path","/api/dwp_jobs2"]
---
+++ app/scrapping.py
@@ -0,0 +1,44 @@
+import os
+
+from dotenv import load_dotenv
+
+from db_functions import engine as engine_db
+from db_functions import get_days as get_days
+from db_functions import main as save_to_db
+from db_functions import read_from_db as read_from_db
+from scrape_functions import get_last_page as get_last_page
+from scrape_functions import iterate_jobs_in_page as iterate_jobs_in_page
+
+load_dotenv()
+
+
+def main():
+    try:
+        # Get connection to database
+        credentials = os.getenv("DATABASE_URL_LOCALHOST")
+        engine = engine_db(credentials)
+        # Get days to scrap and url
+        days = get_days(engine)
+        print(f"Days to scrap: {days}")
+        url = f"https://findajob.dwp.gov.uk/search?pp=50&f={days}"
+        # Scrapping the website
+        last_page = int(get_last_page(url))
+        print(f"Start scrapping total pages: {last_page}")
+        # Iterate through all the pages
+        for page in range(last_page, 0, -1):
+            print("-------------------------------------------------------------")
+            print(f"Start scrapping page: {page}")
+            page_url = f"{url}&page={page}"
+            # scrap each page and save the data in a dataframe
+            jobs_details_for_page = iterate_jobs_in_page(page_url)
+            # jobs_details_for_page.fillna('None', inplace=True)
+            # Save data from each page to db
+            save_to_db(jobs_details_for_page, engine)
+            print(f"| page {page} is saved |")
+        print("Scrapping is done")
+    except Exception as e:
+        print(e)
+
+
+if __name__ == "__main__":
+    main()
